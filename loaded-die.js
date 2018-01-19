bb5940655de3d56b19ad0b4eb5bc4c098d4e3eed
Spencer Greff <spencer.greff@gmail.com>
2018-01-19 00:05:56 +0000

    Build loaded die closure function.

---
+++ loaded-die.js
@@ -0,0 +1,34 @@
+function makeLoadedDie() {
+  var list = [5, 4, 6, 1, 6, 4, 2, 3, 3, 5];
+  var index = 0;
+  var whichRoll;
+  return function() {
+    if (index === list.length) {
+      index = 0;
+    } else if (index === 0) {
+      whichRoll = list[index];
+      index++;
+    } else {
+      whichRoll = list[index];
+      index++;
+    }
+    return whichRoll;
+  };
+}
+var rollLoadedDie = makeLoadedDie();
+console.log(rollLoadedDie());
+console.log(rollLoadedDie());
+console.log(rollLoadedDie());
+console.log(rollLoadedDie());
+console.log(rollLoadedDie());
+console.log(rollLoadedDie());
+console.log(rollLoadedDie());
+console.log(rollLoadedDie());
+console.log(rollLoadedDie());
+console.log(rollLoadedDie());
+console.log(rollLoadedDie());
+console.log(rollLoadedDie());
+console.log(rollLoadedDie());
+console.log(rollLoadedDie());
+console.log(rollLoadedDie());
+console.log(rollLoadedDie());
